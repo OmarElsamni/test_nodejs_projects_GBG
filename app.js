df1162bbfc43b2e4edb4b569307363853d535733
Omar Elsamni <omarelsamni@Omars-MacBook-Pro.local>
2025-11-20 13:19:35 +0200

    added contributors in footer

--- app.js
+++ app.js
@@ -180,9 +180,9 @@ const html = `
     <div class="container">
         <header>
             <nav>
-                <div class="logo">🚀 App Runner</div>
+                <div class="logo">🚀 Samni Elwensh</div>
                 <ul class="nav-links">
-                    <li><a href="#home">samni</a></li>
+                    <li><a href="#home">Home</a></li>
                     <li><a href="#features">Features</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
@@ -218,6 +218,7 @@ const html = `
         
         <footer>
             <p>&copy; 2024 App Runner Application. Powered by AWS.</p>
+            <p>Contributors:Omar Elsamni , Ali Abdelazim , Patol Abdullah</p>
         </footer>
     </div>
 </body>
