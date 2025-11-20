2788ef749b3b31600da29f03699947d47084ad2c
Omar Elsamni <omarelsamni@Omars-MacBook-Pro.local>
2025-11-20 13:12:04 +0200

    test changins in navbar

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ const html = `
             <nav>
                 <div class="logo">🚀 App Runner</div>
                 <ul class="nav-links">
-                    <li><a href="#home">Home</a></li>
+                    <li><a href="#home">samni</a></li>
                     <li><a href="#features">Features</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
